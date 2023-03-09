cfab0eb9ec8b6244057e7f1e5995b56e48a0f414
rafinhacatalano <127411023+rafinhacatalano@users.noreply.github.com>
2023-03-09 10:22:22 -0300

    Add files via upload

---
+++ Pacman.js
@@ -0,0 +1,139 @@
+class Pacman {
+    constructor(x, y, width, height, speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = DIRECTION_RIGHT;
+        this.nextDirection = this.direction;
+        this.currentFrame = 1;
+        this.frameCout = 7;
+
+        setInterval(() => {
+            this.changeAnimation();
+        }, 100);
+    }
+
+    moverProcess() {
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollision()) {
+            this.moveBackwards();
+        }
+
+    }
+
+    eat() {}
+
+    moveBackwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x -= this.speed;
+                break;
+             case DIRECTION_UP:
+                this.y += this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x += this.speed;
+                 break;
+            case DIRECTION_BOTTOM:
+                 this.y -= this.speed;
+                break;
+        }
+    }
+
+    moveForwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x += this.speed;
+                break;
+             case DIRECTION_UP:
+                this.y -= this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed;
+                 break;
+            case DIRECTION_BOTTOM:
+                 this.y += this.speed;
+                break;
+        }
+    }
+
+    checkCollision() {
+        if ( 
+            map[this.getMapY()][this.getMapX()] == 1 ||
+            map[this.getMapYRightSide()][this.getMapX()] == 1 ||
+            map[this.getMapY()][this.getMapXRightSide()] == 1 ||
+            map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+        ) {
+            return true;
+       }
+    return false;
+  }
+
+    checkGhostCollision() {}
+
+    changeDirectionIfPossible() {
+        if(this.direction == this.nextDirection) return
+
+        let tempDirection = this.nextDirection
+        this.direction = this.nextDirection
+        this.moveForwards()
+        if(this.checkCollision()) {
+          this.moveBackwards()  
+        };
+    }
+
+    changeAnimation () {
+        this.currentFrame =
+             this.currentFrame == this.frameCout ? 1 : this.currentFrame + 1;
+    }
+
+    draw() {
+        canvasContext.save()
+        canvasContext.translate(
+            this.x + oneBlockSize / 2,
+            this.y + oneBlockSize / 2
+        );
+        canvasContext.rotate( (this.direction * 90 * Math.PI) / 180);
+
+        canvasContext.translate(
+            -this.x - oneBlockSize / 2,
+            -this.y - oneBlockSize / 2
+        );
+        
+            canvasContext.drawImage(
+            pacmenFrames,
+            (this.currentFrame - 1) * oneBlockSize,
+            0,
+            oneBlockSize,
+            oneBlockSize,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+
+        canvasContext.restore();
+    }
+
+    getMapX() {
+        return parseInt(this.x / oneBlockSize);
+    }
+
+    getMapY() {
+        return parseInt(this.y / oneBlockSize);
+    }
+
+    getMapXRightSide() {
+        return parseInt((this.x + 0.9999 * oneBlockSize ) / oneBlockSize);
+    }
+
+    getMapYightSide() {
+        return parseInt((this.y + 0.9999 * oneBlockSize ) / oneBlockSize);
+    }
+
+
+};
+    
